2d3462da746384e5e203a6bc821e98684ab9d0f7
sanjeev <60956846+sanjeev@users.noreply.github.com>
2024-04-02 18:44:15 +0530

    first commit

--- src/pages/Product/Create.jsx
+++ src/pages/Product/Create.jsx
@@ -8,8 +8,39 @@ import React from 'react';
 import CONFIG from './const';
 
 const Create = () => {
-
-
+  const navigate = useNavigate();
+  const initialValues = {
+    gallery_images: [],
+    featured_images: '',
+    categories: [],
+    tags: [],
+    name: '',
+    slug: '',
+    price: '',
+    short_description: '',
+    long_description: '',
+    status: 'Active',
+    excerpt: '',
+    body: '',
+    notes: '',
+    product_type: '',
+    sale_price: '',
+    quantity: '',
+    sku: '',
+    width: '',
+    height: '',
+    length: '',
+    seo_title: '',
+    seo_keywords: '',
+    seo_description: '',
+  };
+  const onSubmit = async (values, { setSubmitting }) => {
+    console.log(values);
+    // const response = await post(CONFIG.api + 'create', values); // Make a POST request
+    // toast.success(response.message);
+    // navigate(CONFIG.slug);
+    // setSubmitting(false);
+  };
 
   return (
     <>
@@ -20,9 +51,18 @@ const Create = () => {
         />
       </div>
       <div className="">
-
+        {/* <div className="border-b border-stroke p-5   bg-white dark:border-strokedark flex justify-between">
+          <h3 className="font-medium text-black dark:text-white">
+          Create New
+          </h3>
+          <BackButton to={CONFIG.slug} />
+        </div> */}
         <div className="mt-5">
-          wwerwerjlkjlkj
+          <MyForm
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            action={'Create'}
+          />
         </div>
       </div>
     </>
