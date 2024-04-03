e1870b95949d090197b84338d0c1f1c628ac9a8f
sanjeev <60956846+sanjeev@users.noreply.github.com>
2024-04-03 15:49:00 +0530

    first commit

--- src/pages/Product/Create.jsx
+++ src/pages/Product/Create.jsx
@@ -1,38 +1,35 @@
-import Breadcrumb from '../../components/Breadcrumb';
-import { post } from '../../helpers/api';
-import { toast } from 'react-hot-toast';
-import BackButton from '../../components/common/BackButton';
-import MyForm from './Form';
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
-import CONFIG from './const';
+import Breadcrumb from "../../components/Breadcrumb";
+
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import CONFIG from "./const";
 
 const Create = () => {
   const navigate = useNavigate();
   const initialValues = {
     gallery_images: [],
-    featured_images: '',
+    featured_images: "",
     categories: [],
     tags: [],
-    name: '',
-    slug: '',
-    price: '',
-    short_description: '',
-    long_description: '',
-    status: 'Active',
-    excerpt: '',
-    body: '',
-    notes: '',
-    product_type: '',
-    sale_price: '',
-    quantity: '',
-    sku: '',
-    width: '',
-    height: '',
-    length: '',
-    seo_title: '',
-    seo_keywords: '',
-    seo_description: '',
+    name: "",
+    slug: "",
+    price: "",
+    short_description: "",
+    long_description: "",
+    status: "Active",
+    excerpt: "",
+    body: "",
+    notes: "",
+    product_type: "",
+    sale_price: "",
+    quantity: "",
+    sku: "",
+    width: "",
+    height: "",
+    length: "",
+    seo_title: "",
+    seo_keywords: "",
+    seo_description: "",
   };
   const onSubmit = async (values, { setSubmitting }) => {
     console.log(values);
@@ -58,11 +55,11 @@ const Create = () => {
           <BackButton to={CONFIG.slug} />
         </div> */}
         <div className="mt-5">
-          <MyForm
+          {/* <MyForm
             initialValues={initialValues}
             onSubmit={onSubmit}
             action={'Create'}
-          />
+          /> */}
         </div>
       </div>
     </>
